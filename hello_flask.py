d8638de71e2de6fa085fdad95fcfff28693fd309
jai-serr-ace <xilverscott@gmail.com>
2025-03-25 08:44:08 -0700

    Add files via upload

---
+++ hello_flask.py
@@ -0,0 +1,16 @@
+# hello_flask.py
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+# from templates import my_template_1
+
+# create an instance of Flask
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+# route decorator binds a function to a URL
+@app.route('/')
+def hello():
+    return f"<h1>Acronyms</h1> <h1>Oscar: NBA</h1>"
+@app.route('/jaime')
+def t_test():
+    return render_template('my_template_1.html')
